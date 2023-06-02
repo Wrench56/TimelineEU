27e93e3938dfe169588dbc63a9b08dbdc2aae314
Wrench56 <dmarkreg@gmail.com>
2023-06-03 01:41:13 +0200

    Fix ``language`` extraction for translate system

--- javascript/translate.js
+++ javascript/translate.js
@@ -5,7 +5,7 @@ function translate(rel_path_to_lang) {
     else
         filename = document.location.pathname.match(/[^\/]+$/)[0].slice(0, -4) + "json";
     
-    let language = document.location.href.match(/(?<=html[?]lang=).*/)[0];
+    let language = document.location.href.match(/(?<=[?]lang=).*/)[0];
     fetch(`${rel_path_to_lang}/language/${language}/${filename}`)
     .then(response => response.json())
     .then((json) => {
