f7f4ddf00b5da6993e5b299f17cdf79e3db1c00b
Wrench56 <dmarkreg@gmail.com>
2023-06-03 01:33:31 +0200

    Fix translation system when index.html isn't present in the URL

--- javascript/translate.js
+++ javascript/translate.js
@@ -1,5 +1,10 @@
 function translate(rel_path_to_lang) {
-    let filename = document.location.pathname.match(/[^\/]+$/)[0].slice(0, -4) + "json";
+    let filename;
+    if (document.location.pathname.slice(-1) == "/") 
+        filename = "index.json";
+    else
+        filename = document.location.pathname.match(/[^\/]+$/)[0].slice(0, -4) + "json";
+    
     let language = document.location.href.match(/(?<=html[?]lang=).*/)[0];
     fetch(`${rel_path_to_lang}/language/${language}/${filename}`)
     .then(response => response.json())
