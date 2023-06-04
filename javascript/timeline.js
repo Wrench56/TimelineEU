d70d60e39262cb13f5f61a03d03d7c0620f9d611
Wrench56 <dmarkreg@gmail.com>
2023-06-05 00:47:37 +0200

    Add translation for timeline.html

--- javascript/timeline.js
+++ javascript/timeline.js
@@ -1,3 +1,7 @@
+window.onload = (event) => {
+    translate("../");
+};
+
 function load_subpage(page_name) {
     window.location.href = window.location.href.replace("timeline.html", `subpages/${page_name}`);
 }
